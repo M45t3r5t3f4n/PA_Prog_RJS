2baa502209cadc27c2684d455a7f351b02a045a4
M45t3r5t3f4n <stefan.ackermann@edu.teko.ch>
2022-10-01 12:44:58 +0200

    HTML File aufgeräumt

--- src/main/kotlin/ch/teko/todospring/controller/ui/dto/BetragItemDto.kt
+++ src/main/kotlin/ch/teko/todospring/controller/ui/dto/BetragItemDto.kt
@@ -1,8 +1,10 @@
 package ch.teko.todospring.controller.ui.dto
 
+import java.time.Month
+
 class BetragItemDto(
     val id: Long?,
     val title: String,
     val amount: Double,
-    val completed: Boolean
+    val completed: Boolean,
 )
